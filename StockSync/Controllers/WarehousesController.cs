20b309f33a2730363d53807fd41416c9cdaa1a65
Daniel Okafor <austzdee@gmail.com>
2026-06-24 17:12:58 +0100

    Secure warehouse endpoints with authorization

--- StockSync/Controllers/WarehousesController.cs
+++ StockSync/Controllers/WarehousesController.cs
@@ -3,9 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using StockSync.Data;
 using StockSync.DTOs;
 using StockSync.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace StockSync.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class WarehousesController : ControllerBase
